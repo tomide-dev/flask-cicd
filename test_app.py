1677b42e08506878f5995fc67ce152c57a3f589f
Tomide Olubanjo <tomideolubanjo@gmail.com>
2026-03-10 23:13:56 +0000

    Fix flake8 lint errors

--- test_app.py
+++ test_app.py
@@ -8,26 +8,31 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
     data = response.get_json()
     assert data['status'] == 'running'
 
+
 def test_health_route(client):
     response = client.get('/health')
     assert response.status_code == 200
     assert response.get_json()['status'] == 'healthy'
+
 
 def test_add_route(client):
     response = client.get('/add/3/5')
     assert response.status_code == 200
     assert response.get_json()['result'] == 8
 
+
 def test_add_large_numbers(client):
     response = client.get('/add/100/200')
     assert response.status_code == 200
     assert response.get_json()['result'] == 300
+
 
 def test_subtract_route(client):
     response = client.get('/subtract/10/4')
